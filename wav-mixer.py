45e8a684d94f4b30a6c3fd98e70f2628012e2361
Pravir Chandra <chandra@list.org>
2023-04-24 22:23:05 -0400

    add param for setting bit depth of output file

--- wav-mixer.py
+++ wav-mixer.py
@@ -5,6 +5,7 @@ import sys
 import time
 
 SAMPLE_RATE=44100
+BIT_DEPTH = 16
 
 # Timer helper function
 time_start = time.perf_counter()
@@ -24,6 +25,7 @@ This is a really basic tool to mix multiple wav files
 parser = argparse.ArgumentParser(description=DESC)
 parser.add_argument("files", nargs="+", help="input wav files (need at least 2)")
 parser.add_argument("-r", "--rate", required=False, help = "resample all inputs to this rate (default: 44100)")
+parser.add_argument("-b", "--bitdepth", required=False, help = "bits per sample for output file (default: 16)")
 parser.add_argument("-o", "--output", required=True, help = "write wav output to file")
 args = parser.parse_args()
 
@@ -31,6 +33,18 @@ output = args.output
 files = args.files
 if args.rate is not None:
     SAMPLE_RATE = int(args.rate)
+if args.bitdepth is not None:
+    BIT_DEPTH = int(args.bitdepth)
+
+subtype = None
+if BIT_DEPTH == 8:
+    subtype = 'PCM_U8'
+if BIT_DEPTH == 16:
+    subtype = 'PCM_16'
+elif BIT_DEPTH == 24:
+    subtype = 'PCM_24'
+elif BIT_DEPTH == 32:
+    subtype = 'FLOAT'
 
 num = len(files)
 
@@ -70,7 +84,7 @@ for d in data[1:]:
 print_timer()
 print(" - Finished mixing...")
 
-soundfile.write(output, y_out.T, sr)
+soundfile.write(output, y_out.T, sr, subtype=subtype)
 print_timer()
 print(" - Finished writing output to \"" + output + "\"...")
 
